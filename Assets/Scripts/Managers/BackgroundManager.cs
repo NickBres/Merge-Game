7b2ccd32c1dd5cc3a23bd1aba69c0d055b32b71d
NickBres <70432147+NickBres@users.noreply.github.com>
2025-06-12 20:59:56 +0300

    parallax

--- Assets/Scripts/Managers/BackgroundManager.cs
+++ Assets/Scripts/Managers/BackgroundManager.cs
@@ -5,7 +5,9 @@ public class BackgroundManager : MonoBehaviour
 {   
     [SerializeField] private GameObject background;
     [SerializeField] private List<Sprite> backgounds;
+    [SerializeField] private float parallaxStrength = 0.5f;
     private SpriteRenderer currentBackground;
+    private Vector3 initialPosition;
 
     void Awake()
     {
@@ -13,8 +15,19 @@ public class BackgroundManager : MonoBehaviour
         Reset();
     }
 
+    void Start()
+    {
+        initialPosition = background.transform.position;
+    }
+
     private void Reset()
     {
         currentBackground.sprite = backgounds[Random.Range(0, backgounds.Count)];
-    } 
+    }
+
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+        background.transform.position = initialPosition + new Vector3(tilt.x, tilt.y, 0) * parallaxStrength;
+    }
 }
